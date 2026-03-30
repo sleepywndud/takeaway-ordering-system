bc3acd857a43e7d11a63347f47db4645d4efc0e0
wndx2 <w2n2d2@gmail.com>
2026-03-30 15:16:48 +1300

    feat: add voucher button (no functions yet)

--- main.py
+++ main.py
@@ -9,16 +9,32 @@ Project Started on 19.03.2026
 from imports import *
 
 app = Flask(__name__)
+voucher_status = "Not Applied"
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def main():
+    global voucher_status
     conn = sqlite3.connect("database.db")  # connect
-    cr = conn.cursor()  # cursur setting
+    cr = conn.cursor()  # cursor setting
+
     cr.execute("SELECT * FROM pizza")
     data = cr.fetchall()
-    conn.close()
-    return render_template("index.html", data=data)
+
+    if request.method == "POST":
+        if voucher_status == "Voucher Applied":
+            voucher_status = "Not Applied"
+            voucher_button_status = "Apply Voucher"
+        else:
+            voucher_status = "Voucher Applied"
+            voucher_button_status = "Unapply Voucher"
+
+    return render_template(
+        "index.html",
+        data=data,
+        voucher_status=voucher_status,
+        voucher_button_status=voucher_button_status,
+    )
 
 
 if __name__ == "__main__":
